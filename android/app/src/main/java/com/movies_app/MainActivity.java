9ac55eaa863df633f3795031b51ed4083586054f
Yaroslav131 <berserk12186@gmail.com>
2023-09-14 17:56:10 +0300

    add slash screen

--- android/app/src/main/java/com/movies_app/MainActivity.java
+++ android/app/src/main/java/com/movies_app/MainActivity.java
@@ -4,11 +4,14 @@ import com.facebook.react.ReactActivity;
 import com.facebook.react.ReactActivityDelegate;
 import com.facebook.react.defaults.DefaultNewArchitectureEntryPoint;
 import com.facebook.react.defaults.DefaultReactActivityDelegate;
+import org.devio.rn.splashscreen.SplashScreen;
+import android.os.Bundle;
 
 public class MainActivity extends ReactActivity {
 
   /**
-   * Returns the name of the main component registered from JavaScript. This is used to schedule
+   * Returns the name of the main component registered from JavaScript. This is
+   * used to schedule
    * rendering of the component.
    */
   @Override
@@ -16,10 +19,19 @@ public class MainActivity extends ReactActivity {
     return "Movies_App";
   }
 
+  @Override
+  protected void onCreate(Bundle savedInstanceState) {
+    SplashScreen.show(this); 
+    super.onCreate(savedInstanceState);
+  }
+
   /**
-   * Returns the instance of the {@link ReactActivityDelegate}. Here we use a util class {@link
-   * DefaultReactActivityDelegate} which allows you to easily enable Fabric and Concurrent React
-   * (aka React 18) with two boolean flags.
+   * Returns the instance of the {@link ReactActivityDelegate}. Here we use a util
+   * class {@link
+   * DefaultReactActivityDelegate} which allows you to easily enable Fabric and
+   * Concurrent React
+eact
+olean flags.
    */
   @Override
   protected ReactActivityDelegate createReactActivityDelegate() {
